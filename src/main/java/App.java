d9005688dcfaf5ab466d01163618616b7844200d
Denis C <denischirchir@gmail.com>
2022-10-02 17:37:59 +0300

    [2.1.2] 1st commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = applicationContext.getBean("helloworld", HelloWorld.class);
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println(bean2.getMessage());
+
+        System.out.println(bean == bean2);
+
+
+        Cat cat = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(cat == cat2);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,7 @@
+import org.springframework.context.annotation.Scope;
+
+public class Cat {
+    public Cat() {
+        System.out.println("cat created");
+    }
+}
